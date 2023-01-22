0b070611a49549cfffba41303438a9c1f540c961
pabloluceroschneider <pabloluceroschneider@gmail.com>
2023-01-22 15:55:28 -0300

    Add intersection observer

---
+++ js/intersection-observer.js
@@ -0,0 +1,17 @@
+const experienceItems = document.querySelectorAll('.experience__item');
+
+const loadClass = (entrys) => {
+	entrys.forEach((entry) => {
+		if (entry.isIntersecting) {
+      entry.target.classList.add('animate-experience');
+    };
+	});
+};
+
+const observer = new IntersectionObserver(loadClass, {
+	root: null,
+	rootMargin: '0px',
+	threshold: 1.0
+});
+
+experienceItems.forEach((item) => observer.observe(item));
