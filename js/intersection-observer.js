d51a0631dfa2d42db1dfa4690c1184bcc65e4805
pabloluceroschneider <pabloluceroschneider@gmail.com>
2023-03-05 19:23:03 -0300

    remove io

--- js/intersection-observer.js
+++ js/intersection-observer.js
@@ -4,9 +4,7 @@ const loadClass = (entrys) => {
 	entrys.forEach((entry) => {
 		if (entry.isIntersecting) {
       entry.target.classList.add('animation');
-    } else {
-			entry.target.classList.remove('animation');
-		}
+    }
 	});
 };
 
